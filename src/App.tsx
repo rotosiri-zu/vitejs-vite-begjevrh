5cc98dc67d954c85fe494bbf437b395d018d4451
rotosiri-zu <hongjitufang25@gmail.com>
2026-06-11 16:53:54 +0900

    変数の型指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { practice1 } from "./practices/practice1";
 import { practice2 } from "./practices/practice2";
+import { practice3 } from "./practices/practice3";
 import './App.css'
 
 
@@ -11,6 +12,7 @@ function App() {
   <div className="App">
       <practice1 />
       <practice2 />
+      <practice3 />
   </div>
   )
 }
