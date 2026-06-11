2c7d1b9dee80821d0902e5eff63fa92b2d555a77
rotosiri-zu <hongjitufang25@gmail.com>
2026-06-11 15:36:43 +0900

    返却値の型指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { practice1 } from "./practices/practice1";
+import { practice2 } from "./practices/practice2";
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +10,7 @@ function App() {
   return (
   <div className="App">
       <practice1 />
+      <practice2 />
   </div>
   )
 }
