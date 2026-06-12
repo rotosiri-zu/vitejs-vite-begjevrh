c3b7d9983a9d9140f26595e5bcf897d657e0d211
rotosiri-zu <hongjitufang25@gmail.com>
2026-06-12 09:44:16 +0900

    設定ファイル(tsconfig)をいじってみる

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { practice1 } from "./practices/practice1";
 import { practice2 } from "./practices/practice2";
 import { practice3 } from "./practices/practice3";
+import { practice4 } from "./practices/practice4";
 import './App.css'
 
 
@@ -13,6 +14,7 @@ function App() {
       <practice1 />
       <practice2 />
       <practice3 />
+      <practice4 />
   </div>
   )
 }
